536920e96a13f41af14efda58747fb458c146097
= <57465032+SoraAsc@users.noreply.github.com>
2025-09-26 00:34:18 -0300

    fix: hyprland settings

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -2,24 +2,20 @@
 {
   programs = {
     kitty.enable = true;
-    quickshell = {
-      enable = true;
-      activeConfig = "./quickshell/";
-    };
+    #quickshell = {
+    #  enable = true;
+    #  activeConfig = "./quickshell/";
+    #};
   };
   wayland.windowManager.hyprland = {
     enable =  true;
     #xwayland.enable = true;
     settings = {
-      "$mainMod" = "SUPER";
-      "$terminal" = "kitty";
       debug = {
         disable_logs = false;
         enable_stdout_logs = false;
       };
-      monitor = {
-        ",preferred,auto,1,mirror,edP-1"
-      };
+      monitor = ",preferred,auto,1,mirror,edP-1";
       input = {
         kb_layout = "br";
         kb_variant = "abnt2";
@@ -29,19 +25,19 @@
         follow_mouse = 1;
         #touchpad.natural_scroll = false;
         touchpad.disable_while_typing = 0;
-        sensitivity 0;
       };
       gestures = {
-        gesture = 3, horizontal, workspace
+        gesture = "3, horizontal, workspace";
       };
-      bind = {
-        "$mainMod Return, exec, $terminal"
-        "$mainMod Q, killactive"
-        "$mainMod X, exit"
-        "$mainMod P, togglefloating"
-        "$mainMod J, togglesplit"
-        "$mainMod F, fullscreen"
-
+      "$mainMod" = "SUPER";
+      "$terminal" = "kitty";
+      bind = [
+        "$mainMod, return, exec, $terminal"
+        "$mainMod, Q, killactive"
+        "$mainMod, X, exit"
+        "$mainMod, P, togglefloating"
+        "$mainMod, J, togglesplit"
+        "$mainMod, F, fullscreen"
         "$mainMod, 1, workspace, 1"
         "$mainMod, 2, workspace, 2"
         "$mainMod, 3, workspace, 3"
@@ -52,7 +48,6 @@
         "$mainMod, 8, workspace, 8"
         "$mainMod, 9, workspace, 9"
         "$mainMod, 0, workspace 10"
-
         "$mainMod SHIFT, 1, movetoworkspace, 1"
         "$mainMod SHIFT, 2, movetoworkspace, 2"
         "$mainMod SHIFT, 3, movetoworkspace, 3"
@@ -63,7 +58,7 @@
         "$mainMod SHIFT, 8, movetoworkspace, 8"
         "$mainMod SHIFT, 9, movetoworkspace, 9"
         "$mainMod SHIFT, 0, movetoworkspace, 10"
-      };
+      ];
     };
   };
 }
